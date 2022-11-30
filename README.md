5e4d9992cd06eb895d441f004d8f608c66f6162d
ex_lihj8 <ex_lihj8@cnooc.com.cn>
2022-11-30 11:45:20 +0800

    更新readme文档及引入typescript

--- README.md
+++ README.md
@@ -4,16 +4,22 @@
 - vue3 **√** 
 - vuex **√**
 - vue-router **√**
-- typescript
+- typescript **√**
 - vant(创建vant分支引入移动端ui组件)
 - element-ui(创建element分支引入pc端ui组件)
 - eslint
 - prettier
 
 ### 优化配置 ###
-- 打包优化bundle文件分离式缓存
 - 路由单页面懒加载 **√**
+- 打包优化bundle文件分离式缓存
 - 单项目多入口共享文件
+- 优化typescript定义支持及各项配置
+- 优化vue-router自动解析目录文件生成
+- 优化vuex多文件分包集成
+- 优化eslint配置
+- 优化prettier配置
+- 优化webpack各项配置及热更新优化
 
 **以上打√的是已经配置在项目里的，接下来会逐步配置引入，引入完成后重点在于eslint prettier和各项打包优化配置**
 
--- build/webpack.config.js
+++ build/webpack.config.js
@@ -66,13 +66,38 @@ module.exports = {
 				test: /\.tsx?$/,
 				exclude: /node_modules/,
 				use: {
-          loader: 'ts-loader',
-          options: {
-            // onlyCompileBundledFiles: true,
-            // transpileOnly: true,
-            appendTsSuffixTo: [/\.vue$/],
-          },
-        },
+					loader: 'babel-loader',
+					options: {
+						presets: [
+							[
+								"@babel/preset-env",
+								{
+									targets: {
+										browsers: ["ie>=8", "chrome>=62"],
+										node: "8.9.0",
+									},
+									debug: false,
+									useBuiltIns: "usage",
+									corejs: "3.0",
+								},
+							],
+							[
+								"@babel/preset-typescript",
+								{
+									allExtensions: true, // 支持所有文件扩展名，否则在vue文件中使用ts会报错
+								},
+							],
+						],
+						plugins: [
+							[
+								"@babel/plugin-transform-runtime",
+								{
+									corejs: 3,
+								},
+							],
+						],
+					},
+				},
 			},
 			{
 				test: /\.m?js$/,
